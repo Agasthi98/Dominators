e9f2bd1f43c7f3fd50f99af093844ba6e0792485
ushiniavindika <ushavindika@gmail.com>
2020-10-01 10:19:54 +0530

    cart item updated

--- app/src/main/java/com/example/dominatorsmad_project/CartItemViewHolder.java
+++ app/src/main/java/com/example/dominatorsmad_project/CartItemViewHolder.java
@@ -9,23 +9,25 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CartItemViewHolder extends RecyclerView.ViewHolder {
+
     public ImageView cartItemImage;
     public TextView cartItemName;
     public TextView cartItemPrice;
-    public Button cDeleteButton;
-    public Button cAddButton;
-    public Button removeButton;
-    public TextView cQuantity;
+    public Button cartItemDeleteButton;
+    public Button addItemButton;
+    public Button removeItemButton;
+    public TextView cartItemQuantity;
 
     public CartItemViewHolder(@NonNull View itemView) {
         super(itemView);
-        cartItemImage = itemView.findViewById(R.id.display_cake_image);
+        cartItemImage = itemView.findViewById(R.id.display_cart_cake_image);
         cartItemName = itemView.findViewById(R.id.display_cart_item_name);
-        cartItemPrice = itemView.findViewById(R.id.display_cake_price);
-        cDeleteButton = itemView.findViewById(R.id.cart_item_delete_button);
-        cAddButton = itemView.findViewById(R.id.cart_item_add_button);
-        removeButton = itemView.findViewById(R.id.cart_item_remove_button);
-        cQuantity = itemView.findViewById(R.id.cart_item_quantity);
+        cartItemPrice = itemView.findViewById(R.id.display_cart_item_price);
+        cartItemDeleteButton = itemView.findViewById(R.id.cart_item_delete_button);
+        addItemButton = itemView.findViewById(R.id.cart_item_add_button);
+        removeItemButton = itemView.findViewById(R.id.cart_item_remove_button);
+        cartItemQuantity = itemView.findViewById(R.id.cart_item_quantity);
     }
-
 }
+
+
