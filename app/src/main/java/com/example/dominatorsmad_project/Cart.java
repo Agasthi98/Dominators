51f91afccbc2806f20c63336d223ef5287cf406b
ushiniavindika <ushavindika@gmail.com>
2020-10-01 10:18:13 +0530

    cart updated

--- app/src/main/java/com/example/dominatorsmad_project/Cart.java
+++ app/src/main/java/com/example/dominatorsmad_project/Cart.java
@@ -1,22 +1,23 @@
 package com.example.dominatorsmad_project;
 
 public class Cart {
+
     private String cartId;
     private String cakeName;
     private String cakePrice;
     private String cakeImage;
+    private String cakeDescription;
     private int cakeQuantity;
     private int totPrice;
 
-    public Cart() {
+    public Cart() {}
 
-    }
-
-    public Cart(String cartId, String cakeName, String cakePrice, String cakeImage, int cakeQuantity, int totPrice) {
+    public Cart(String cartId, String cakeName, String cakePrice, String cakeImage, String cakeDescription, int cakeQuantity, int totPrice) {
         this.cartId = cartId;
         this.cakeName = cakeName;
         this.cakePrice = cakePrice;
         this.cakeImage = cakeImage;
+        this.cakeDescription = cakeDescription;
         this.cakeQuantity = cakeQuantity;
         this.totPrice = totPrice;
     }
@@ -35,6 +36,10 @@ public class Cart {
 
     public String getCakeImage() {
         return cakeImage;
+    }
+
+    public String getCakeDescription() {
+        return cakeDescription;
     }
 
     public int getCakeQuantity() {
